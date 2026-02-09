b94a1096c32dcafa0ca32eb5c8cd8556e7ebef02
Rey <89825425+kingg22@users.noreply.github.com>
2026-02-09 17:16:50 -0500

    misc: rename contributing md

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-rootProject.name = "api-vacunas-panama"
-
 pluginManagement {
     repositories {
         mavenCentral()
@@ -17,3 +15,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+rootProject.name = "api-vacunas-panama"
